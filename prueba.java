632d89eae5aa406c00a22b20c6df9c57f10fd37c
Daniel Soto Jaimes <danielsoto@uninorte.edu.co>
2021-11-09 17:13:04 -0500

    my change

--- prueba.java
+++ prueba.java
@@ -1,6 +1,2 @@
-public class AnyThing{
-    public static void main (String[] args){
-        int a = 2;
-        double b = 2 + 3;
-    }
-}
+int a = 2;
+double b = 3;
--- y.tab.h
+++ y.tab.h
@@ -1,14 +1,13 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.0.4.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +15,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,66 +30,75 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     STRING = 258,
-     ID = 259,
-     INT = 260,
-     REAL = 261,
-     BOOLEAN = 262,
-     CHARID = 263,
-     IFELSE = 264,
-     FOR = 265,
-     WHILE = 266,
-     DOUBLE = 267,
-     INTW = 268,
-     STRINGW = 269,
-     BOOLEANW = 270,
-     CHARNEW = 271,
-     PUBLIC = 272,
-     CLASS = 273,
-     STATIC = 274,
-     VOID = 275,
-     CHAR = 276,
-     IF = 277,
-     ELSE = 278,
-     NEW = 279,
-     COMMENT = 280,
-     SUMSUM = 281,
-     MENMEN = 282,
-     SUMIGUAL = 283,
-     MENIGUAL = 284,
-     MULTIGUAL = 285,
-     DIVIGUAL = 286,
-     IGUALIGUAL = 287,
-     MENORIGUAL = 288,
-     MAYORIGUAL = 289,
-     DIFF = 290,
-     NOIGUAL = 291,
-     YY = 292,
-     OO = 293,
-     NOT = 294,
-     MAYOR = 295,
-     MENOR = 296,
-     MULT = 297,
-     SUM = 298,
-     MEN = 299,
-     DIV = 300,
-     IGUAL = 301,
-     MOD = 302,
-     PAper = 303,
-     PCier = 304,
-     CAper = 305,
-     CCier = 306,
-     LAper = 307,
-     LCier = 308,
-     PComa = 309,
-     SALTOLINEA = 310
-   };
+  enum yytokentype
+  {
+    STRING = 258,
+    ID = 259,
+    INT = 260,
+    REAL = 261,
+    BOOLEAN = 262,
+    CHARID = 263,
+    IFELSE = 264,
+    FOR = 265,
+    WHILE = 266,
+    DOUBLE = 267,
+    INTW = 268,
+    STRINGW = 269,
+    BOOLEANW = 270,
+    CHARNEW = 271,
+    PUBLIC = 272,
+    CLASS = 273,
+    STATIC = 274,
+    VOID = 275,
+    CHAR = 276,
+    IF = 277,
+    ELSE = 278,
+    NEW = 279,
+    COMMENT = 280,
+    SUMSUM = 281,
+    MENMEN = 282,
+    SUMIGUAL = 283,
+    MENIGUAL = 284,
+    MULTIGUAL = 285,
+    DIVIGUAL = 286,
+    IGUALIGUAL = 287,
+    MENORIGUAL = 288,
+    MAYORIGUAL = 289,
+    DIFF = 290,
+    NOIGUAL = 291,
+    YY = 292,
+    OO = 293,
+    NOT = 294,
+    MAYOR = 295,
+    MENOR = 296,
+    MULT = 297,
+    SUM = 298,
+    MEN = 299,
+    DIV = 300,
+    IGUAL = 301,
+    MOD = 302,
+    PAper = 303,
+    PCier = 304,
+    CAper = 305,
+    CCier = 306,
+    LAper = 307,
+    LCier = 308,
+    PComa = 309,
+    SALTOLINEA = 310
+  };
 #endif
 /* Tokens.  */
 #define STRING 258
@@ -149,29 +155,30 @@
 #define PComa 309
 #define SALTOLINEA 310
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef int YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
 
-extern YYSTYPE yylval;
-
+/* Location type.  */
 #if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
-typedef struct YYLTYPE
+typedef struct YYLTYPE YYLTYPE;
+struct YYLTYPE
 {
   int first_line;
   int first_column;
   int last_line;
   int last_column;
-} YYLTYPE;
-# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
+};
 # define YYLTYPE_IS_DECLARED 1
 # define YYLTYPE_IS_TRIVIAL 1
 #endif
 
+
+extern YYSTYPE yylval;
 extern YYLTYPE yylloc;
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
